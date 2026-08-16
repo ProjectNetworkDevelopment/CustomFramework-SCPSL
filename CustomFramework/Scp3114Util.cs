82e29ddabc1fa40eb74c821779c52d8d1f27a0b9
Pyro Cyclone <cyclonepyro@gmail.com>
2026-08-16 14:54:47 -0600

    Added ForceAbortStrangle extension method.

---
+++ CustomFramework/Scp3114Util.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using LabApi.Features.Wrappers;
+using PlayerRoles.PlayableScps.Scp3114;
+using System;
+
+namespace CustomFramework
+{
+    public static class Scp3114Util
+    {
+        public static void ForceAbortStrangle(this Scp3114Role role)
+        {
+            if (role.SubroutineModule.TryGetSubroutine(out Scp3114Strangle call))
+            {
+                var syncTarget = AccessTools.Method(typeof(Scp3114Strangle), "set_SyncTarget");
+                var rpcType = AccessTools.Field(typeof(Scp3114Strangle), "_rpcType");
+                var rpcTypeEnum = rpcType.FieldType;
+                var sendRpc = AccessTools.Method(typeof(Scp3114Strangle), "ServerSendRpc", new Type[] { typeof(bool) });
+
+                syncTarget.Invoke(call, new object[] { null });
+                rpcType.SetValue(call, Enum.Parse(rpcTypeEnum, "AttackInterrupted"));
+                sendRpc.Invoke(call, new object[] { true });
+            }
+        }
+    }
+}
